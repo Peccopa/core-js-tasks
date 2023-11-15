7b4f39fa1499ad3a8472af64738f25c6b7872626
Andrey Zharkikh <orbital.logistics@yandex.ru>
2023-11-15 12:39:08 +0300

    init: start task strings

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -19,8 +19,9 @@
  *   getStringLength(null) => 0
  *   getStringLength(undefined) => 0
  */
-function getStringLength(/* value */) {
-  throw new Error('Not implemented');
+function getStringLength(value) {
+  return value ? value.length : 0;
+  // throw new Error('Not implemented');
 }
 
 /**
