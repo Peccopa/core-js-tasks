193c7fe66f14cb84a03b4af48b1160f6b074b5ea
Andrey Zharkikh <orbital.logistics@yandex.ru>
2023-12-24 18:05:44 +0300

    feat: simple-chain task complete

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,26 +5,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  arr : [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.arr.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    value !== undefined ? value : value = '';
+    if (this.arr.length === 0) {
+      this.arr.push(`( ${value} )`);
+      return this;
+    }
+    this.arr.push(`( ${value} )`);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (isNaN(position) || !this.arr[position - 1]) {
+      this.arr = [];
+      throw new Error(`You can't remove incorrect link!`);
+    }
+    this.arr.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.arr.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    const result = this.arr.join('~~');
+    this.arr = [];
+    return result;
+  },
 };
 
 module.exports = {
