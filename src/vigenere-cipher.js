97a6a0d9b32ec59600b3d528f26e061f1ffc8091
Andrey Zharkikh <orbital.logistics@yandex.ru>
2023-12-25 12:26:42 +0300

    feat: vigenere-cipher task complete

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,64 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(direction = true) {
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    this.direction = direction;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+    if (this.direction) {
+      key = key.toUpperCase();
+      message = message.toUpperCase();
+    } else {
+      key = key.toUpperCase();
+      message = message.split('').reverse().join('');
+    }
+    let result = '';
+    let tmp = 0;
+    for (let i = 0; i < message.length; i++) {
+      let current = message[i];
+      if (i >= message.length) current = message[i % message.length];
+      if (this.alphabet.includes(current)) {
+        const indexA = this.alphabet.indexOf(current);
+        let indexK = this.alphabet.indexOf(key[tmp >= key.length ? tmp % key.length : tmp]);
+        const char = this.alphabet[(this.alphabet.length + (indexA + indexK)) % this.alphabet.length];
+        result += char;
+        tmp++;
+      } else {
+        result += current;
+      }
+    }
+    return result;
+  }
+
+  decrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+    if (this.direction) {
+      message = message.toUpperCase();
+      key = key.toUpperCase();
+    } else {
+      message = message.split('').reverse().join('').toUpperCase();
+      key = key.toUpperCase();
+    }
+    let result = '';
+    let tmp = 0;
+    for (let i = 0; i < Math.max(message.length); i++) {
+      let current = message[i];
+      if (i >= message.length) current = message[i % message.length];
+      if (this.alphabet.includes(current)) {
+        const indexA = this.alphabet.indexOf(current);
+        let indexK = this.alphabet.indexOf(key[tmp >= key.length ? tmp % key.length : tmp]);
+        const char = this.alphabet[(this.alphabet.length + indexA - indexK) % this.alphabet.length];
+        result += char;
+        tmp++;
+      } else {
+        result += current;
+      }
+    }
+    return result;
   }
 }
 
